1854f3c75ab2d96380d5d9f05651571d9a094f2b
Saroj Parajuli <saroj.parajuli91@gmail.com>
2022-04-03 14:13:52 -0500

    updated for marketing deployment

--- marketing/src/helpers/helperPricing.js
+++ marketing/src/helpers/helperPricing.js
@@ -1,12 +1,12 @@
 export function Copyright() {
-    return (
-      <Typography variant="body2" color="textSecondary" align="center">
-        {'Copyright © '}
-        <Link color="inherit" href="https://material-ui.com/">
-          Your Website
-        </Link>{' '}
-        {new Date().getFullYear()}
-        {'.'}
-      </Typography>
-    );
-  }
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
